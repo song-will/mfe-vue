819606832f7ba43d0b5829db895532a765f68741
zhuqingsong <735560190@qq.com>
2022-04-16 13:24:12 +0800

    feat: publicPath

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,11 @@ const { defineConfig } = require('@vue/cli-service')
 const { name } = require('./package.json')
 module.exports = defineConfig({
   transpileDependencies: true,
+  output: {
+    publicPath: process.env.NODE_ENV === 'development'
+    ? '/'
+    : `/mfe/vue/`
+  },
   devServer: {
     port: 8082,
     headers: {
